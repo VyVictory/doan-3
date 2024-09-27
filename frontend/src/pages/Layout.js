9730910eeb7ee1b4d15ada7d35052786b68b689e
TuanVux <tuanvukg2703@gmail.com>
2024-09-27 16:57:29 +0700

    dropdown notification bg layout

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -3,7 +3,7 @@ import NavBar from "../components/navbar/navBar";
 import Footer from "../components/footer/Footer"
 export default function Layout() {
     return (
-        <div className=" h-full max-h-screen  text-[#f8fafc]">
+        <div className=" h-full max-h-screen bg-[#020617]  text-[#f8fafc]">
             <NavBar />
             <div className="  mb-5 pt-16">
                 <Outlet />
