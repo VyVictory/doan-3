bc5543fed195867d9cd56276480bcf6541e63631
VyVictory <vyvictory2003@gmail.com>
2024-09-29 16:03:22 +0700

    messchane

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -2,10 +2,11 @@ import { Outlet, Link } from "react-router-dom";
 import NavBar from "../components/navbar/navBar";
 import Footer from "../components/footer/Footer"
 export default function Layout() {
+
     return (
-        <div className=" h-full min-h-screen bg-[#020617]  text-[#f8fafc]">
+        <div className=" h-full min-h-screen bg-[#020617] text-[#f8fafc]" >
             <NavBar />
-            <div className="  mb-5 pt-16">
+            <div className="pt-16">
                 <Outlet />
             </div>
             <Footer />
--- frontend/src/pages/messenger/LeftMessenger.jsx
+++ frontend/src/pages/messenger/LeftMessenger.jsx
@@ -1,12 +1,36 @@
 
-const LeftMessenger = () => {
-    return ( <>
-        
-            <div className="">
+const LeftMessenger = ({bt_chanetransfer}) => {
+    const messages = [
+        { name: 'John Doe', message: 'Hey! How are you?', img: 'https://placehold.co/40' },
+        { name: 'Jane Smith', message: 'Hi there!', img: 'https://placehold.co/40' },
+        { name: 'Alice Johnson', message: 'Good morning!', img: 'https://placehold.co/40' },
+        { name: 'Bob Brown', message: 'What’s up?', img: 'https://placehold.co/40' },
+        { name: 'Charlie Davis', message: 'Long time no see!', img: 'https://placehold.co/40' },
+        { name: 'Diana Prince', message: 'Let’s catch up!', img: 'https://placehold.co/40' },
+        { name: 'Ethan Hunt', message: 'Ready for the mission!', img: 'https://placehold.co/40' },
 
+    ];
+    return (<>
+            <div class="w-full md:w-1/4   border-r-gray-400 border-r-2 text-white float-end">
+                <div className="p-4">
+                    <h2 className="text-lg font-semibold mb-4">Chat History</h2>
+                    <ul className=''>
+                        {messages.map((msg, index) => (
+                            <li key={index}>
+                                <button onClick={bt_chanetransfer} className="flex items-center py-2">
+                                    <img src={msg.img} alt="user" className="w-10 h-10 rounded-full mr-2" />
+                                    <div className='text-start'>
+                                        <h3 className="text-primary font-semibold">{msg.name}</h3>
+                                        <p className="text-secondary">{msg.message}</p>
+                                    </div>
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        
-    </> );
+
+    </>);
 }
- 
+
 export default LeftMessenger;
