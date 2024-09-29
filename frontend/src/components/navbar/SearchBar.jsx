f8315f547e9b832da98ae9162dbef05c0e268237
TuanVux <tuanvukg2703@gmail.com>
2024-09-29 14:42:28 +0700

    SearchBar

---
+++ frontend/src/components/navbar/SearchBar.jsx
@@ -0,0 +1,49 @@
+'use client';
+import React from 'react';
+import { useState } from 'react';
+import { Button } from '@headlessui/react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import { Input } from '@headlessui/react';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+export default function SearchBar({ query }) {
+    const [searchTerm, setSearchTerm] = useState("");
+    //Search underfi
+    if (query === '') {
+        return null;
+    }
+    //REST API
+
+    //Không tìm thấy
+    // if (data.length === 0) {
+    //     return <p>Không tìm thấy từ khóa: <i>"{query}"</i></p>;
+    // }
+    // input
+    // Xử lý sự kiện khi thay đổi input
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    // Xử lý sự kiện khi nhấn nút xóa
+    const handleClearSearch = () => {
+        setSearchTerm("");
+    };
+    return (
+        <form action="" className="relative w-full max-w-sm justify-center">
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleInputChange}
+                className=" w-72 rounded-xl pr-10 pl-10 py-2 text-black"
+                placeholder="Tìm kiếm..."
+            />
+            {searchTerm && (
+                <button onClick={handleClearSearch} className='absolute right-7 top-[40%] transform -translate-y-1/2 text-gray-400 h-4 w-4'>
+                    <XMarkIcon className="size-6 fill-black" />
+                </button>
+            )}
+            <button className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" type="submit"><MagnifyingGlassIcon className='size-5 fill-black' /></button>
+            {/* onChange={(e) => debounceSearch(e.target.value)}
+             defaultValue={searchParams.get("query")?.toString()} */}
+        </form >
+    )
+}
