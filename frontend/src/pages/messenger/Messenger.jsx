04957ec4ba095819e45292b62853d734238da9c6
VyVictory <vyvictory2003@gmail.com>
2024-10-05 21:22:34 +0700

    thêm nút thoát chat khi size điện thoại

--- frontend/src/pages/messenger/Messenger.jsx
+++ frontend/src/pages/messenger/Messenger.jsx
@@ -65,10 +65,19 @@ const Messenger = () => {
                 {(chanecontainer || transfer == false) && (
                     <div className={`${transfer ? 'w-3/4' : 'w-full'}  h-full `} >
                         <div className='flex flex-col h-full' >
+
                             <div className=" p-2 shadow-sm flex flex-row items-center border-b border-gray-200">
+                                {(transfer == false) &&
+                                    <div className='pt-2 px-2'>
+                                        <button onClick={backtransfer}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                                <path fillRule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z" clipRule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                }
                                 <img src="https://placehold.co/40" alt="user" className="w-10 h-10 rounded-full mr-2" />
                                 <h3 className=" font-semibold">Jane Smith</h3>
-                                {textareaHeight}
                             </div>
                             <div className="overflow-y-scroll h-full p-4 pt-2 flex flex-col"
                                 style={{
@@ -94,13 +103,13 @@ const Messenger = () => {
                                     </ul>
                                 </div>
                             </div>
-                             <div className={`w-full flex mb-1 pt-1 px-1 border-t border-gray-200`}> {/* ${textareaHeight > 56 ? 'flex-col' : 'flex-row'} */}
+                            <div className={`w-full flex mb-1 pt-1 px-1 border-t border-gray-200`}> {/* ${textareaHeight > 56 ? 'flex-col' : 'flex-row'} */}
                                 <div className={` flex items-center justify-center`}>
                                     <button className='h-8 w-8'>
                                         😄
                                     </button>
                                 </div>
-                            
+
                                 <div className='px-1 w-full flex justify-center'>
                                     <Textarea
                                         className={clsx(
@@ -110,12 +119,12 @@ const Messenger = () => {
                                         rows={1}
                                         style={{ height: textareaHeight > 56 ? `${textareaHeight}px` : 'auto', maxHeight: '4rem', minHeight: '40px' }} // Sử dụng textareaHeight
                                         placeholder='nhập @, tin nhắn'
-                                    onInput={(e) => {
-                                        const textarea = e.target;
-                                        textarea.style.height = 'auto'; // Reset chiều cao
-                                        textarea.style.height = `${textarea.scrollHeight}px`; // Cập nhật chiều cao
-                                        setTextareaHeight(textarea.scrollHeight); // Cập nhật chiều cao cho state
-                                    }}
+                                        onInput={(e) => {
+                                            const textarea = e.target;
+                                            textarea.style.height = 'auto'; // Reset chiều cao
+                                            textarea.style.height = `${textarea.scrollHeight}px`; // Cập nhật chiều cao
+                                            setTextareaHeight(textarea.scrollHeight); // Cập nhật chiều cao cho state
+                                        }}
                                     />
                                 </div>
 
