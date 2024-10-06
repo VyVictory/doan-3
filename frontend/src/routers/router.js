aebd82d3efd15f394233835e46d4bee1fc88ab55
TuanVux <tuanvukg2703@gmail.com>
2024-10-06 09:31:44 +0700

    fix not find file Navbar

--- frontend/src/routers/router.js
+++ frontend/src/routers/router.js
@@ -11,7 +11,7 @@ import MyPosts from "../components/Profile/MyPosts.jsx";
 import FriendProfile from "../components/Profile/FriendProfile.jsx";
 import Messenger from "../pages/messenger/Messenger.jsx";
 import Searchpage from "../pages/search/index.jsx";
-import Navbar from "../components/navbar/Navbar.jsx"
+import Navbar from "../components/navbar/navBar.jsx";
 import Allfriend from "../components/friend/Allfriend.jsx";
 import Friendinvitation from "../components/friend/friendinvitation.jsx";
 
@@ -22,28 +22,28 @@ function routers() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<Layout />}>
-                            <Route index element={<Home />} />
-                            <Route path="friends" element={<Myfriend />} >
-                                <Route path="list" element={<Allfriend />} />
-                                <Route path="requests" element={<Friendinvitation />} />
-                            </Route>
-                            <Route path="search" element={<Searchpage />} />
-                            <Route path="user" element={<Personal />}>
-                                <Route index element={<MyPosts />} />
-                                <Route path="about" element={<AboutProfile />} />
-                                <Route path="friends" element={<FriendProfile />} />
-                            </Route>
-                            {/* <Route path="*" element={<NoPage />} /> */}
-                            <Route path="messenger" element={<Messenger />} />
-                      
+                        <Route index element={<Home />} />
+                        <Route path="friends" element={<Myfriend />} >
+                            <Route path="list" element={<Allfriend />} />
+                            <Route path="requests" element={<Friendinvitation />} />
+                        </Route>
+                        <Route path="search" element={<Searchpage />} />
+                        <Route path="user" element={<Personal />}>
+                            <Route index element={<MyPosts />} />
+                            <Route path="about" element={<AboutProfile />} />
+                            <Route path="friends" element={<FriendProfile />} />
+                        </Route>
+                        {/* <Route path="*" element={<NoPage />} /> */}
+                        <Route path="messenger" element={<Messenger />} />
+
                     </Route>
                     <Route path="auth" element={<Authencation />}>
                         <Route index element={<Authencation />} />
                         {/* <Route path="*" element={<NoPage />} /> */}
                     </Route>
-                   
+
                 </Routes>
-               
+
             </BrowserRouter>
         </div>
     );
