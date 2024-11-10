38b57ebe828de2d12d501ee4d289b9a8afeb7073
TuanVux <tuanvukg2703@gmail.com>
2024-11-10 15:02:13 +0700

    /fix UI/UX modal form POST status

--- frontend/src/views/post/components/ModalStatus.jsx
+++ frontend/src/views/post/components/ModalStatus.jsx
@@ -2,6 +2,8 @@ import { Textarea } from '@headlessui/react'
 import { useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import clsx from 'clsx'
+import { PhotoIcon } from '@heroicons/react/24/solid'
+import DropdownPrivacy from './DropdownPrivacy'
 
 export default function ModalStatus({ status }) {
     const [open, setOpen] = useState(true)
@@ -19,42 +21,70 @@ export default function ModalStatus({ status }) {
                         transition
                         className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
                     >
-                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                            <div className="sm:flex sm:items-start">
-                                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                    <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                                        Bạn muốn đăng gì ?
-                                    </DialogTitle>
-                                    <div className="mt-2">
-                                        <Textarea
-                                            className={clsx(
-                                                'mt-3 block w-full min-w-[420px] resize-none rounded-lg border-2 border-black bg-white/5 py-1.5 px-3 text-sm/6 text-black',
-                                                'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-                                            )}
-                                            rows={3}
-                                            placeholder='Viết nội dung của bạn...'
-                                        />
+                        <form method="post">
+                            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                <div className="sm:flex sm:items-start">
+                                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                        <DialogTitle as="h1" className="text-xl font-bold leading-10 grid justify-center text-gray-900">
+                                            Tạo bài viết
+                                        </DialogTitle>
+                                        <div className="mt-2">
+                                            <label htmlFor="text-area" className='block text-sm text-gray-700 font-semibold'>
+                                                Bạn muốn đăng gì hôm nay?
+                                            </label>
+                                            <Textarea
+                                                id='text-area'
+                                                className={clsx(
+                                                    'block w-full min-w-[420px] resize-none rounded-lg border-2 border-black bg-white/5 py-1.5 px-3 text-sm/6 text-black',
+                                                    'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                                                )}
+                                                rows={5}
+                                                placeholder='Viết nội dung của bạn...'
+                                            />
+                                            <div className="mt-4 flex items-center gap-5">
+                                                <label className="block text-sm font-medium text-gray-700">
+                                                    Ai có thể xem bài viết của bạn?
+                                                </label>
+                                                <DropdownPrivacy  />
+                                            </div>
+                                            <div className="mt-4 flex items-center gap-5">
+                                                <label className="block text-sm font-medium text-gray-700">
+                                                    Thêm ảnh vào bài viết của bạn
+                                                </label>
+                                                <div className="file-input-wrapper ">
+                                                    <input
+                                                        type="file"
+                                                        accept="image/*"
+                                                        className="hidden"
+                                                        id="file-input"
+                                                    />
+                                                    <label htmlFor="file-input" className="file-input-button">
+                                                        <PhotoIcon className='size-9 fill-sky-600 ' />
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                            <button
-                                type="button"
-                                onClick={status}
-                                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                            >
-                                Đăng bài
-                            </button>
-                            <button
-                                type="button"
-                                data-autofocus
-                                onClick={status}
-                                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                            >
-                                Hủy đăng
-                            </button>
-                        </div>
+                            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                <button
+                                    type="submit"
+
+                                    className="inline-flex w-full justify-center rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 sm:ml-3 sm:w-auto"
+                                >
+                                    Đăng bài
+                                </button>
+                                <button
+                                    type="button"
+                                    data-autofocus
+                                    onClick={status}
+                                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                >
+                                    Hủy đăng
+                                </button>
+                            </div>
+                        </form>
                     </DialogPanel>
                 </div>
             </div>
