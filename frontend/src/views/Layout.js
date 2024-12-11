d43cefc3ddb40bf20072734ddd3b999fbd7d49f9
VyVictory <vyvictory2003@gmail.com>
2024-12-11 19:37:09 +0700

    fix

--- frontend/src/views/Layout.js
+++ frontend/src/views/Layout.js
@@ -2,10 +2,10 @@ import { Outlet, Link, useNavigate  } from "react-router-dom";
 import Navbar from "./navbar/navBar";
 import LeftListMenu from "./menu/LeftMenuList";
 import authToken from "../components/authToken";
+import { useEffect } from "react";
 
 export default function Layout() {
     const navigate = useNavigate();
-
     useEffect(() => {
         if (!authToken.getToken()) {
             navigate("/login");
