b5bc0b5c4a70f31dcc8e3180b99884726453077c
VyVictory <vyvictory2003@gmail.com>
2024-12-14 17:30:41 +0700

    sửa lại cái nút user

--- frontend/src/views/search/userCard/cardUserList.jsx
+++ frontend/src/views/search/userCard/cardUserList.jsx
@@ -14,27 +14,35 @@ const CardUserList = ({ userdata }) => {
             console.error(error);
         }
     };
+    const handDetailUser = async (id) => {
+        window.location.href = `/user/${id}`;
+    };
     return (
-        <div className="w-full flex flex-row  justify-between items-center p-2 max-h-[80px] sm:max-h-[60px] md:max-h-[70px] lg:max-h-[80px]">
+        <button
+            onClick={() => { handDetailUser(userdata._id) }}
+            className="w-full flex flex-row rounded-lg hover:bg-green-50 justify-between items-center p-2 max-h-[80px] sm:max-h-[60px] md:max-h-[70px] lg:max-h-[80px]">
             <div className="flex flex-row items-center">
                 <div className="w-14 h-14 rounded-full">
                     <img src={userdata && userdata.img ? userdata.img : userImg} />
                 </div>
                 <div className="flex flex-col pl-2">
-                    <div>{userdata ? (userdata.firstName ? userdata.firstName : '', userdata.lastName ? userdata.lastName : '') : 'No Name'}</div>
+                    <div className='text-start'>{userdata ? (userdata.firstName ? userdata.firstName : '', userdata.lastName ? userdata.lastName : '') : 'No Name'}</div>
                     <div>number post</div>
                     {/* Friend chung */}
                 </div>
             </div>
             <div className="py-5">
                 <button
-                    onClick={userdata && userdata._id ? () => handAddFriend(userdata._id) : undefined}
-                    className="bg-blue-200 rounded-xl p-2 min-w-24"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        if (userdata && userdata._id) handAddFriend(userdata._id);
+                    }}
+                    className="bg-blue-100 rounded-xl p-2 min-w-24 hover:bg-blue-200 hover:text-blue-600 text-blue-500"
                 >
-                    Kết bạn
+                    <strong className='text-sm'>Add Friend</strong>
                 </button>
             </div>
-        </div>
+        </button>
     );
 }
 
