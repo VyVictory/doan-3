83bb9becb9c3a4eb49c501fb33eb33b6d156222c
TuanVux <tuanvukg2703@gmail.com>
2024-12-20 13:30:39 +0700

    background and friend

--- frontend/src/views/friend/FriendProfile.jsx
+++ frontend/src/views/friend/FriendProfile.jsx
@@ -1,61 +1,44 @@
 import React from "react"
-import DropdownFriendProfile from "../profile/components/DropdownFriendProfile"
 import DropdownMyfriend from "./DropdownMyfriend"
-
+import { useState, useEffect } from "react"
+import friend from "../../service/friend"
+import Loading from "../../components/Loading"
 export default function FriendProfile() {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      setLoading(true);
+      const response = await friend.getListMyFriend();
+      setData(response.data)
+      setLoading(false);
+    }
+    setTimeout(fetchdata, 1000);
+  }, [])
   return (
     <ul className="grid gap-3 sm:grid-cols-2 sm:gap-y-3 xl:col-span-2 p-3 ">
-      <li className='border-2 border-black rounded-md p-2 '>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-x-6">
-            <img alt="" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className="h-16 w-16 rounded-full" />
-            <div>
-              <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900 ">Leslie Alexander</h3>
-              <p className="text-sm font-semibold leading-6 text-indigo-600">Co-Founder / CEOd</p>
-            </div>
-          </div>
-          <DropdownMyfriend />
-        </div>
-      </li>
-      {/* 2 */}
-      <li className='border-2 border-black rounded-md p-2 '>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-x-6">
-            <img alt="" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className="h-16 w-16 rounded-full" />
-            <div>
-              <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900 ">Leslie Alexander</h3>
-              <p className="text-sm font-semibold leading-6 text-indigo-600">Co-Founder / CEOd</p>
-            </div>
-          </div>
-          <DropdownMyfriend />
-        </div>
-      </li>
-      {/* 3 */}
-      <li className='border-2 border-black rounded-md p-2 '>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-x-6">
-            <img alt="" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className="h-16 w-16 rounded-full" />
-            <div>
-              <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900 ">Leslie Alexander</h3>
-              <p className="text-sm font-semibold leading-6 text-indigo-600">Co-Founder / CEOd</p>
-            </div>
-          </div>
-          <DropdownMyfriend />
-        </div>
-      </li>
-      {/* 4 */}
-      <li className='border-2 border-black rounded-md p-2 '>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-x-6">
-            <img alt="" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className="h-16 w-16 rounded-full" />
-            <div>
-              <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900 ">Leslie Alexander</h3>
-              <p className="text-sm font-semibold leading-6 text-indigo-600">Co-Founder / CEOd</p>
-            </div>
-          </div>
-          <DropdownMyfriend />
-        </div>
-      </li>
+      {loading ? (
+        <Loading />
+      ) : (
+        data.length > 0 ? (
+          data.map((e) => (
+            <li className='border-[1px] rounded-md p-2 shadow-lg'>
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-x-6 ">
+                  <img
+                    className="w-14 aspect-square rounded-full shadow-md"
+                    alt=""
+                    src={`${e.receiver.avatar}`} />
+                  <div>
+                    <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900 ">{e.receiver.firstName} {e.receiver.lastName}</h3>
+                  </div>
+                </div>
+                <DropdownMyfriend />
+              </div>
+            </li>
+          ))
+        ) : (<span>Chưa có bạn bè</span>))}
     </ul>
 
   )
