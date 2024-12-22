2551a72b7eec630a20389ebd93b050e249ed4ca5
TuanVux <tuanvukg2703@gmail.com>
2024-12-22 20:31:02 +0700

    dont

---
+++ frontend/src/views/post/components/CommentReply.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react'
+import { getComment, handleLike, handleUnLike } from '../../../service/CommentService';
+import AVTUser from '../AVTUser';
+import { Link } from 'react-router-dom';
+import { differenceInMinutes, differenceInHours, differenceInDays } from 'date-fns';
+
+export default function CommentReply({ open, postId, user, cmtId }) {
+    const [comment, setComment] = useState([])
+
+    useEffect(() => {
+        const fetchdata = async () => {
+            try {
+                const response = await getComment(postId)
+                if (response) {
+                    const sortedComments = response.data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+                    setComment(sortedComments)
+                }
+            } catch (error) {
+                console.error("Error liking the post:", error);
+            }
+        }
+        fetchdata()
+    }
+        , [postId]);
+
+    //format Time CreateAt Comment
+    const formatDate = (date) => {
+        const postDate = new Date(date);
+        const currentDate = new Date();
+        const minutesDifference = differenceInMinutes(currentDate, postDate);
+        const hoursDifference = differenceInHours(currentDate, postDate);
+        const daysDifference = differenceInDays(currentDate, postDate);
+
+        if (minutesDifference < 60) {
+            return `${minutesDifference} phút trước`;
+        } else if (hoursDifference < 24) {
+            return `${hoursDifference} giờ trước`;
+        } else if (daysDifference <= 30) {
+            return `${daysDifference} ngày trước`;
+        } else {
+            return postDate.toLocaleDateString('vi-VN');
+        }
+    };
+    //Like
+    const handleLikeClick = async (cmtId) => {
+        try {
+            const cmt = comment.find(e => e._id === cmtId)
+            if (cmt.likes.includes(user._id)) {
+                // Optimistically update the UI
+                cmt.likes = cmt.likes.filter(id => id !== user._id)
+                setComment([...comment])
+                await handleUnLike(cmtId);
+            } else {
+                // Optimistically update the UI
+                cmt.likes = [...cmt.likes, user._id]
+                setComment([...comment])
+                await handleLike(cmtId);
+            }
+        } catch (error) {
+            console.error("Error liking the post:", error);
+        }
+    }
+    console.log(comment[0]?.replyTo.length)
+    return (
+        <>
+            {open === true && (
+                <div className='bg-blue-200 mt-5'>
+                    {comment.length === 0 ? (
+                        <div className="text-center p-4">No data</div>
+                    ) : (
+                        comment.filter(cmt => cmt?.replyTo[0] === cmtId).length === 0 ? (
+                            <div className="text-center p-4">No data</div>
+                        ) : (
+                            comment.map((cmt) => (
+                                cmt?.replyTo[0] === cmtId && (
+                                    <div key={cmt._id} className="bg-card dark:bg-card-foreground p-4 rounded-lg rounded-b-none border-b-2 ">
+                                        <div className=' '>
+                                            <div className="flex items-center gap-2 ">
+                                                {/* <img className="h-12 w-12 rounded-full mr-4" src="https://placehold.co/50x50" alt="user-avatar" /> */}
+                                                <AVTUser user={cmt?.author} />
+                                                <div>
+                                                    <Link to={`/user/${cmt?.author?._id}`} className="text-lg font-semibold">{cmt?.author?.lastName} {cmt?.author?.firstName}</Link>
+                                                    <p className="text-sm text-muted-foreground">{formatDate(cmt.createdAt)}</p>
+                                                </div>
+                                            </div>
+                                            <p className="text-base mt-4">{cmt?.content}</p>
+                                            <div className="flex items-center justify-between mt-4">
+                                                <div className='flex gap-1'>
+                                                    <span>{cmt?.likes?.length}</span>
+                                                    <button onClick={() => handleLikeClick(cmt?._id)} className={"flex items-end gap-1"}>
+                                                        {cmt?.likes?.includes(user._id)
+                                                            ? <div className='text-blue-700 animate__heartBeat'>Like</div>
+                                                            : <div className='text-gray-700 '>Like</div>
+                                                        }
+                                                    </button>
+                                                </div>
+                                                {/* <button onClick={() => handleReply(e._id)} className="text-secondary">Phản hồi</button> */}
+                                            </div>
+                                        </div>
+                                        {/* <FormReply open={openReplyId === e._id} keycmt={e} /> */}
+                                    </div>
+                                )
+                            ))
+                        )
+                    )}
+                </div>
+            )}
+        </>
+    )
+}
