d628a26ae95f4c287e4319743164fafc350611cb
VyVictory <vyvictory2003@gmail.com>
2024-12-23 20:00:47 +0700

    logic hơn

--- frontend/src/views/messenger/layoutMessenger.js
+++ frontend/src/views/messenger/layoutMessenger.js
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LeftMessenger from "./components/LeftMessenger";
 import RightMessenger from "./components/rightMessenger";
 import { createContext } from "react";
-import { ToastContainer } from "react-toastify";
+import { useLocation } from "react-router-dom";
 export const MessengerContext = createContext();
-
 const LayoutMessenger = () => {
     const [RightShow, setRightShow] = useState(true);
+    const location = useLocation()
     const [content, setContent] = useState(true);
     const [inboxData, setInboxData] = useState({
         data: [],
@@ -16,6 +16,22 @@ const LayoutMessenger = () => {
     const handleHiddenRight = () => {
         setRightShow(!RightShow)
     }
+    useEffect(() => {
+        function updateContentBasedOnURL() {
+            const urlParams = new URL(window.location.href); // Lấy URL hiện tại
+            const pathSegment = urlParams.pathname.split('/')[2]; // Lấy phần sau /messenger/
+
+            if (pathSegment === 'friend') {
+                setContent('inbox');
+            } else if (pathSegment === 'group') {
+                setContent('group');
+            } else {
+                console.warn('Không nhận diện được path segment:', pathSegment);
+            }
+        }
+        updateContentBasedOnURL()
+
+    }, [location]);
     return (
         <MessengerContext.Provider value={{ RightShow, handleHiddenRight, content, setContent, setInboxData, inboxData }}>
             <div className="h-screen flex flex-row bg-gray-100 text-black" style={{ marginTop: '-68px', paddingTop: '68px' }}>
