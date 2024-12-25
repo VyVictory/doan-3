78e40b059e0563a0849a01cdea60d1899c13e2c5
TuanVux <tuanvukg2703@gmail.com>
2024-12-25 09:31:46 +0700

    fix gender

--- frontend/src/views/search/postSearch.jsx
+++ frontend/src/views/search/postSearch.jsx
@@ -1,24 +1,25 @@
 import { useState, useEffect } from "react";
 import { getSearchResult } from "../../service/SearchService";
-
+import { useParams } from "react-router-dom";
 const PostSearch = () => {
     const [search, setSearch] = useState([]);
-
+    const { content } = useParams();
 
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const response = await getSearchResult("");
+                const response = await getSearchResult(content);
                 setSearch(response);
             } catch (error) {
                 console.error(error);
             }
         };
         fetchPosts();
-    }, []);
+    }, [content]);
     return (
         <div className="mt-5">
-            <span className="mb-1">Kết quả tìm kiếm</span>
+            <span className="mb-1">Kết quả tìm kiếm từ khóa: {content}</span>
+
             <div className="card card-side max-h-[200px] bg-base-100 shadow-xl">
                 <figure>
                     <img
@@ -26,8 +27,8 @@ const PostSearch = () => {
                         alt="Movie" />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">Tên người dùng</h2>
-                    <p>Content</p>
+                    <h2 className="card-title">{search.firstName} {search.lastName}</h2>
+                    <p>{search.content}</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Xem</button>
                     </div>
