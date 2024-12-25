782ae7fb628ce328eac3ee68bfe4a225546d1796
VyVictory <vyvictory2003@gmail.com>
2024-12-25 13:25:43 +0700

    vs

--- frontend/src/views/messenger/components/rightMessenger.jsx
+++ frontend/src/views/messenger/components/rightMessenger.jsx
@@ -1,15 +1,6 @@
 import { useState, useEffect } from "react";
-import ToggleButton from "@mui/material/ToggleButton";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import { InboxIcon, UserGroupIcon, UsersIcon, ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
-import { Button, Typography, Box, IconButton, Modal } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
-
-import AllFriend from "./leftMenu/allFriend";
-import AllGroup from "./leftMenu/allGroup";
-import AllInbox from "./leftMenu/allInbox";
 import ToolInbox from "./rightMenu/toolInbox";
 import ToolGroup from "./rightMenu/toolGroup";
 
@@ -17,11 +8,6 @@ import { MessengerContext } from "../layoutMessenger";
 
 const RightMessenger = () => {
     const { content } = useContext(MessengerContext);
-    const [alignment, setAlignment] = useState("inbox");
-    const navigate = useNavigate(); // React Router navigation function
-    const handleChange = (event, newAlignment) => {
-        setAlignment(newAlignment);
-    };
 
     const renderContent = () => {
         switch (content) {
